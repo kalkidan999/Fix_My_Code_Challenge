8dc263b8c883827e5fcdd63a7d7166d247659e20
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-08-01 06:21:51 +0300

    Delete declaration

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -13,8 +13,7 @@ if (process.argv.length <= 2) {
     process.stderr.write("Example: ./1-print_square.js 8\n");
     process.exit(1)
 }
-let x = 10;
-size = parseInt(process.argv[2], x)
+size = parseInt(process.argv[2], 10)
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
